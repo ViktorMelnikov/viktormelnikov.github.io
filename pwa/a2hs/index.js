7a9a726fe9118dd5fe4cb430192925e3f497a41a
Melnikov VIktor <v.melnikov@propellerads.net>
2020-04-09 14:41:50 +0300

    WIP

--- pwa/a2hs/index.js
+++ pwa/a2hs/index.js
@@ -25,6 +25,21 @@ let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 // addBtn.style.display = 'none';
 
+//*********************************************************
+// Detects if device is on iOS
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
+//*********************************************************
+
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
